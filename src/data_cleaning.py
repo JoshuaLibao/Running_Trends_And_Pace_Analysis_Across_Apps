302f990ef90dfe371e981462d53471b108aa81a1
Joshua Libao <fsshockwave@gmail.com>
2026-03-11 22:32:57 -0700

    data cleaning
    started the cleaning process of the nike run club data.

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -16,18 +16,18 @@ db_path = r'C:\Users\12063\Downloads\sqlite\strava_nike_performance_analysis\dat
 conn = sqlite3.connect(db_path)
 
 strava_path = r'C:\Users\12063\Downloads\sqlite\strava_nike_performance_analysis\data\raw\activities.csv'
-df = pd.read_csv(strava_path)
+strava_raw = pd.read_csv(strava_path)
 
 #change duplicate column name
-cols = list(df.columns)
+cols = list(strava_raw.columns)
 
 # confirm all values are whole numbers in duplicate column -> clear to delete column
-elapsed_time_check = (df['Elapsed Time.1'] % 1 == 0).all()
-df = df.drop(df.columns[15], axis=1)
+elapsed_time_check = (strava_raw['Elapsed Time.1'] % 1 == 0).all()
+strava_raw = strava_raw.drop(strava_raw.columns[15], axis=1)
 
 # standardize columns to snake_case
-df.columns = (
-    df.columns
+strava_raw.columns = (
+    strava_raw.columns
         .str.strip()
         .str.lower()
         .str.replace(" ", "_")
@@ -35,18 +35,17 @@ df.columns = (
 )
 
 # validate: date, time, distance and activity type
-
-df['activity_date'] = pd.to_datetime(
-    df['activity_date'],
+strava_raw['activity_date'] = pd.to_datetime(
+    strava_raw['activity_date'],
     format= "%b %d, %Y, %I:%M:%S %p",
 )
 
 today = pd.Timestamp.today()
-clean_strava_data = df[
-    (df['activity_date'] <= today) &
-    (df['elapsed_time'] > 0) &
-    (df['distance'] > 0) &
-    (df['activity_type'] == 'Run')
+clean_strava_data = strava_raw[
+    (strava_raw['activity_date'] <= today) &
+    (strava_raw['elapsed_time'] > 0) &
+    (strava_raw['distance'] > 0) &
+    (strava_raw['activity_type'] == 'Run')
 ]
 clean_strava_data.to_sql('clean_strava_data', conn, if_exists='replace', index=False)
 
@@ -54,3 +53,21 @@ input_dir = r'C:\Users\12063\Downloads\sqlite\strava_nike_performance_analysis\d
 output_csv = r'C:\Users\12063\Downloads\sqlite\strava_nike_performance_analysis\data\processed\nrc_tcx_summary.csv'
 
 write_summary_csv(input_dir, output_csv)
+
+nike_path = r'C:\Users\12063\Downloads\sqlite\strava_nike_performance_analysis\data\processed\nrc_tcx_summary.csv'
+nike_raw = pd.read_csv(nike_path)
+
+# standardize data
+nike_raw['start_time_utc'] = pd.to_datetime(
+    nike_raw['start_time_utc'],
+    format= "%Y-%m-%dT%H:%M:%S.%fZ"
+)
+
+nike_raw['start_date'] = nike_raw['start_time_utc'].dt.date
+nike_raw['start_time'] = nike_raw['start_time_utc'].dt.time
+
+
+# clean_nike_data = nike_raw[
+#     (nike_raw[])
+
+# ]
