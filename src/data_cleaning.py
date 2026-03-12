73465b14081ca717fb030bf864edf6263616b1ac
Joshua Libao <fsshockwave@gmail.com>
2026-03-12 16:20:50 -0700

    standardized data and continued data cleaning
    * standardized strava and nike run club column names
    * validated nike run club data (timestamp validation)

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -25,7 +25,7 @@ cols = list(strava_raw.columns)
 elapsed_time_check = (strava_raw['Elapsed Time.1'] % 1 == 0).all()
 strava_raw = strava_raw.drop(strava_raw.columns[15], axis=1)
 
-# standardize columns to snake_case
+# standardize columns
 strava_raw.columns = (
     strava_raw.columns
         .str.strip()
@@ -39,12 +39,19 @@ strava_raw['activity_date'] = pd.to_datetime(
     strava_raw['activity_date'],
     format= "%b %d, %Y, %I:%M:%S %p",
 )
+strava_raw = strava_raw.rename(columns={
+    'activity_date':'timestamp', 
+    'elapsed_time':'total_time_s',
+    'distance':'distance_m',
+    'average_heart_rate':'avg_hr_bpm',
+    'max_heart_rate.1':'max_hr_bpm'
+    })
 
-today = pd.Timestamp.today()
+now = pd.Timestamp.now()
 clean_strava_data = strava_raw[
-    (strava_raw['activity_date'] <= today) &
-    (strava_raw['elapsed_time'] > 0) &
-    (strava_raw['distance'] > 0) &
+    (strava_raw['timestamp'] <= now) &
+    (strava_raw['total_time_s'] > 0) &
+    (strava_raw['distance_m'] > 0) &
     (strava_raw['activity_type'] == 'Run')
 ]
 clean_strava_data.to_sql('clean_strava_data', conn, if_exists='replace', index=False)
@@ -57,17 +64,26 @@ write_summary_csv(input_dir, output_csv)
 nike_path = r'C:\Users\12063\Downloads\sqlite\strava_nike_performance_analysis\data\processed\nrc_tcx_summary.csv'
 nike_raw = pd.read_csv(nike_path)
 
-# standardize data
+# standardize nike run club data
 nike_raw['start_time_utc'] = pd.to_datetime(
     nike_raw['start_time_utc'],
     format= "%Y-%m-%dT%H:%M:%S.%fZ"
 )
 
-nike_raw['start_date'] = nike_raw['start_time_utc'].dt.date
-nike_raw['start_time'] = nike_raw['start_time_utc'].dt.time
+nike_raw = nike_raw.rename(columns={
+    'start_time_utc':'timestamp',
+    'average_cadence_rpm':'average_cadence'
+    })
 
+nike_raw['start_date'] = nike_raw['timestamp'].dt.date
+nike_raw['start_time'] = nike_raw['timestamp'].dt.time
+nike_raw['timestamp'] = nike_raw['timestamp'].dt.strftime("%Y-%m-%d %H:%M:%S")
 
-# clean_nike_data = nike_raw[
-#     (nike_raw[])
+# validate nike run club data
+# nike_clean_data = nike_raw[
+#     (nike_raw['timestamp'] <= now) &
 
 # ]
+
+
+print(nike_raw)
