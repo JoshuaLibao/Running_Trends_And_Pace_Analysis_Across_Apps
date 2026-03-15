6e22f71017eb24b6c0958c08f4801e3d3daee264
Joshua Libao <fsshockwave@gmail.com>
2026-03-14 23:26:27 -0700

    validated and standardized data
    * validated nike run club data
    * standardized distance unit in the nike data from meters to miles
    * standardized the entry for activity_type in the strava data
    * standardized the timestamp in nike data: formatting and timezone
    * standardized column names between the strava and nike run club data

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -18,9 +18,6 @@ conn = sqlite3.connect(db_path)
 strava_path = r'C:\Users\12063\Downloads\sqlite\strava_nike_performance_analysis\data\raw\activities.csv'
 strava_raw = pd.read_csv(strava_path)
 
-#change duplicate column name
-cols = list(strava_raw.columns)
-
 # confirm all values are whole numbers in duplicate column -> clear to delete column
 elapsed_time_check = (strava_raw['Elapsed Time.1'] % 1 == 0).all()
 strava_raw = strava_raw.drop(strava_raw.columns[15], axis=1)
@@ -34,7 +31,6 @@ strava_raw.columns = (
         .str.replace("-", "_")
 )
 
-# validate: date, time, distance and activity type
 strava_raw['activity_date'] = pd.to_datetime(
     strava_raw['activity_date'],
     format= "%b %d, %Y, %I:%M:%S %p",
@@ -42,18 +38,21 @@ strava_raw['activity_date'] = pd.to_datetime(
 strava_raw = strava_raw.rename(columns={
     'activity_date':'timestamp', 
     'elapsed_time':'total_time_s',
-    'distance':'distance_m',
+    'distance':'distance_miles',
     'average_heart_rate':'avg_hr_bpm',
     'max_heart_rate.1':'max_hr_bpm'
     })
+strava_raw['activity_type'] = strava_raw['activity_type'].replace('Run','running')
 
+# validate: date, time, distance and activity type
 now = pd.Timestamp.now()
 clean_strava_data = strava_raw[
     (strava_raw['timestamp'] <= now) &
     (strava_raw['total_time_s'] > 0) &
-    (strava_raw['distance_m'] > 0) &
-    (strava_raw['activity_type'] == 'Run')
+    (strava_raw['distance_miles'] > 0) &
+    (strava_raw['activity_type'] == 'running')
 ]
+strava_raw.to_sql('strava_raw', conn, if_exists='replace', index=False)
 clean_strava_data.to_sql('clean_strava_data', conn, if_exists='replace', index=False)
 
 input_dir = r'C:\Users\12063\Downloads\sqlite\strava_nike_performance_analysis\data\raw\tcx'
@@ -65,25 +64,41 @@ nike_path = r'C:\Users\12063\Downloads\sqlite\strava_nike_performance_analysis\d
 nike_raw = pd.read_csv(nike_path)
 
 # standardize nike run club data
-nike_raw['start_time_utc'] = pd.to_datetime(
-    nike_raw['start_time_utc'],
-    format= "%Y-%m-%dT%H:%M:%S.%fZ"
-)
-
 nike_raw = nike_raw.rename(columns={
     'start_time_utc':'timestamp',
-    'average_cadence_rpm':'average_cadence'
+    'average_cadence_rpm':'average_cadence',
+    'sport':'activity_type',
+    'distance_m':'distance_miles'
     })
+nike_raw['timestamp'] = pd.to_datetime(
+    nike_raw['timestamp']
+)
+# convert timestamp column from: timezone-aware -> timezone-native
+nike_raw['timestamp'] = nike_raw['timestamp'].dt.tz_localize(None)
 
+# separate start date and time
 nike_raw['start_date'] = nike_raw['timestamp'].dt.date
 nike_raw['start_time'] = nike_raw['timestamp'].dt.time
-nike_raw['timestamp'] = nike_raw['timestamp'].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+# standardize distance units: meters -> miles
+nike_raw['distance_miles'] = nike_raw['distance_miles'] / 1609.34
 
 # validate nike run club data
-# nike_clean_data = nike_raw[
-#     (nike_raw['timestamp'] <= now) &
+clean_nike_data = nike_raw[
+    (nike_raw['timestamp'] <= now) &
+    (nike_raw['total_time_s'] > 0) &
+    (nike_raw['distance_miles'] > 0) &
+    (nike_raw['activity_type'] == 'Running')
+]
 
-# ]
+# standardize timestamp entry format
+clean_nike_data['timestamp'] = clean_nike_data['timestamp'].dt.strftime("%Y-%m-%d %H:%M:%S")
 
+nike_raw.to_sql('nike_raw', conn, if_exists='replace', index=False)
+clean_nike_data.to_sql('nike_clean_data', conn, if_exists='replace', index=False)
 
-print(nike_raw)
+combined_datasets = pd.read_sql("""
+    SELECT timestamp, distance_miles, total_time_s, activity_type, total_time_s/distance_miles AS pace
+    FROM 
+
+""", conn)
